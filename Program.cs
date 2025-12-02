5400d084cdf60940974498d6bd5d6136d538eba6
jhonnydevbr <jhonnybrustolin@gmail.com>
2025-12-02 19:05:45 -0300

    organized the project with the folder and apiconfig file

--- Program.cs
+++ Program.cs
@@ -1,97 +1,14 @@
 using System.Text;
-using apifuncional.Data;
-using apifuncional.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using apifuncional.Configuration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development", builder =>
-                    builder
-                            .AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader());
-    options.AddPolicy("Production", builder =>
-                    builder
-                            .WithOrigins("https://localhost:7200")
-                            .WithMethods("POST", "GET", "DELETE", "PUT")
-                            .AllowAnyHeader());
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string []{}
-        }
-    });
-
-});
-
-
-builder.Services.AddDbContext<ApiDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApiDbContext>();
-
-// Pegar token e gerando chave encodada
-var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
-builder.Services.Configure<JwtSettings>(JwtSettingsSection);
-
-var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
-var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
-
-// Autenticação
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = true;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audiencia,
-        ValidIssuer = jwtSettings.Emissor
-    };
-});
+builder.AddApiConfig()
+       .AddCorsConfig()
+       .AddSwaggerConfig()
+       .AddDbContextConfig()
+       .AddIdentityConfig();
 
 var app = builder.Build();
 
