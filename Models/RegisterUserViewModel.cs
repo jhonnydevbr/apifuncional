f9f2a7f3f9b5ed26c2d0fb60cedf863e66273892
jhonnydevbr <jhonnybrustolin@gmail.com>
2025-12-02 16:58:50 -0300

    adding jwt support in Swagger

--- Models/RegisterUserViewModel.cs
+++ Models/RegisterUserViewModel.cs
@@ -8,13 +8,13 @@ public class RegisterUserViewModel
 
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
     [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
 
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
     [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     [Compare("Password", ErrorMessage = "As senhas não conferem.")]
-    public string ConfirmPassword { get; set; }
+    public required string ConfirmPassword { get; set; }
 }
